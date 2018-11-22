65e2377685120f3d0a47959aedc6dee8e56a776d
iconojdiazch <khepherer@gmail.com>
2018-11-22 02:02:14 +0100

    Repo reorganizado

---
+++ ejemplo/main.js
@@ -0,0 +1,9 @@
+window.addEventListener(
+    "load",
+    () =>{
+        const lista = document.getElementsByTagName("a");        
+        for( elem of lista){
+            elem.style.display="block";
+        }
+    }
+);
