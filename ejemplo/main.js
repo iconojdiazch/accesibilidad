a542f480c1b3c6620ffc0aae28110cbd310e730c
iconojdiazch <khepherer@gmail.com>
2018-11-23 00:30:52 +0100

    ejemplo modificado

--- ejemplo/main.js
+++ ejemplo/main.js
@@ -1,9 +1,20 @@
 window.addEventListener(
     "load",
     () =>{
-        const lista = document.getElementsByTagName("a");        
-        for( elem of lista){
-            elem.style.display="block";
-        }
+        const lista = document.querySelectorAll("a");     
+        Array.prototype.forEach.call(
+            lista,
+            elem => elem.style.display="block"
+        );   
+        // for( elem of lista){
+        //     elem.style.display="block";
+        // }
+        const nodo = document.querySelector("#skip");                
+        nodo.addEventListener(
+            "focus",
+            () =>{
+                console.log("El enlace invisible ha recibido el foco");                
+            }
+        );
     }
 );
